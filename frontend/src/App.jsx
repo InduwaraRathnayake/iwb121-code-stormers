105743dd8e7454cbc0b9b0c6f0e1f7cb9259679b
Pathumi Ranasinghe <162596287+PathumiRanasinghe@users.noreply.github.com>
2024-09-26 12:30:15 +0530

    updated slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,9 @@
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"; 
+
 
 import Home from "./pages/Home";
 import Services from "./pages/Services";
