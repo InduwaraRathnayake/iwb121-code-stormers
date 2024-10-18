269b181fc1cfa377d36ed2d5da754ea708cce34a
Induwara Rathnayake <rathnayakeinduwara23@gmail.com>
2024-10-18 23:29:43 +0530

    Finalized schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,12 +1,37 @@
-CREATE DATABASE  IF NOT EXISTS "wellness" ;
+CREATE DATABASE  IF NOT EXISTS "wellness" /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `wellness`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: mysql-2c283d8b-wellness-360.l.aivencloud.com    Database: wellness
+-- ------------------------------------------------------
+-- Server version	8.0.30
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '1c40ca4b-81b3-11ef-b932-02c022ded385:1-102';
 
 --
 -- Table structure for table `contact_form`
 --
 
 DROP TABLE IF EXISTS `contact_form`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contact_form` (
   `id` int NOT NULL AUTO_INCREMENT,
   `subject` varchar(255) DEFAULT NULL,
@@ -16,13 +41,25 @@ CREATE TABLE `contact_form` (
   `email` varchar(255) DEFAULT NULL,
   `message` text,
   PRIMARY KEY (`id`)
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact_form`
+--
+
+LOCK TABLES `contact_form` WRITE;
+/*!40000 ALTER TABLE `contact_form` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact_form` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -33,4 +70,27 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'johndoe','John','Doe','johndoe@gmail.com',_binary '}¸SÇuÅ≠ıï¥\Í∞†\Î¡~x\‡j®π4ë\Ÿ_∆øQtê\«\‚uB√•2	8S(N£¶˜|¡x\Óî¯\◊be\\∫_u\ÏDeP\Ê\ÈÃÑr®Z≤á\‘…ΩIƒö\∆\Á$?\⁄\Ë˘±X©òiı‡º∑ÿº\‚\ÁF\‚UT\÷\Î\"πk—®û»π\Âß¿_4∂\ÃN[Ñ\’“§˚p%\„nurh«£*vKH∂\’3˙è\À/\Ê≠›∂x≤OQ\€$-\∆@D¡†§høâ8\Õw:ß@KIx\–jy\Ê\”>™e0Sî˝o)P®\≈rm3èd_u^Úå¬â}{´\–\–\–˝í˚\Ó2g‹£$y8ÅÇâ\√');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-18 23:27:49
